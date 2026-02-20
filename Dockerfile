95a278918dbec1325caff0a49371b99f70d5e6e3
Geradine <ehfy2005@gmail.com>
2026-02-21 01:04:23 +0300

    Собрал образ докером(dockerfile) подправил compose, сделал локальный для проверки жизнеспособности

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# 1 BUILD STAGE (Java 21)
+FROM gradle:8.5-jdk21 AS build
+WORKDIR /app
+
+# Copy entire project
+COPY . .
+
+# Build project (skip tests inside Docker)
+RUN ./gradlew build --no-daemon -x test
+
+
+# 2 RUNTIME STAGE (Java 21)
+FROM gcr.io/distroless/java21-debian12:nonroot
+WORKDIR /opt/eureka
+
+# Copy jar from APP module
+COPY --from=build /app/app/build/libs/*.jar app.jar
+
+EXPOSE 8080
+
+CMD ["-jar", "app.jar"]
--- infrastructure/src/main/java/com/example/config/SecurityConfig.java
+++ infrastructure/src/main/java/com/example/config/SecurityConfig.java
@@ -65,7 +65,9 @@ public class SecurityConfig {
             "http://localhost:5500",
             "http://127.0.0.1:5500",
             "http://localhost:63342",
-            "http://127.0.0.1:63342"
+            "http://127.0.0.1:63342",
+            "http://localhost:8080",
+            "http://127.0.0.1:8080"
         ));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"));
